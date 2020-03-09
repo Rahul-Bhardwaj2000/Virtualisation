b007b5ad28b359d7a94d9e2eb79998e0da2a15c5
Rahul-Bhardwaj2000 <rahul265653@gmail.com>
2020-03-09 14:40:12 +0530

    Added server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+import json
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    print(conn,s)
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            data=json.loads(data)
+            print(data)
