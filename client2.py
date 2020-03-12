5cc90e66fa76f744ce982c73d1751d36bbee9f45
Rahul-Bhardwaj2000 <rahul265653@gmail.com>
2020-03-12 22:28:08 +0530

    added server and client

---
+++ client2.py
@@ -0,0 +1,39 @@
+import socket
+import json
+import time
+HOST = '127.0.0.1'
+# HOST = '192.168.122.24'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+
+MAX_SIZE=1024
+WaitTime=0.01
+def GetRequest(key,s):
+	s.sendall(str.encode("GET"))
+	ack=s.recv(MAX_SIZE).decode()
+	if ack=="OK":
+		s.sendall(str.encode(key))
+		value=s.recv(MAX_SIZE).decode()
+
+
+def StartClient(s):
+	counter=0
+	while True:
+		key="1"
+		ans=GetRequest(key,s)
+		time.sleep(WaitTime)
+		counter+=1
+		if counter%1000==0:
+			print(counter," requests done")
+
+
+def CreateConnection():
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	return s
+
+
+s=CreateConnection()
+StartClient(s)
+
+
--- server.py
+++ server.py
@@ -1,18 +1,44 @@
 import socket
 import json
-HOST = '192.168.122.24'  # Standard loopback interface address (localhost)
+
+HOST = '127.0.0.1'
+# HOST = '192.168.122.24'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+
+MAX_SIZE=1024
+key_value_pairs={}
+key_value_pairs["1"]="rahul"
+
+
+def HandleGet(conn):
+	key=conn.recv(MAX_SIZE).decode()
+	conn.sendall(str.encode(str(key_value_pairs[key])))
+
+def HandlePost(conn):
+	data=conn.recv(MAX_SIZE)
+	data=json.loads(data)
+	for key in data.keys():
+		key_value_pairs[key]=data[key]
+	conn.sendall(str.encode("DONE"))
+
+def HandleRequest(conn,data):
+	conn.sendall(str.encode("OK"))
+	if data=="GET":
+		HandleGet(conn)
+	if data=="POST":
+		HandlePost(conn)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    print(conn,s)
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            data=json.loads(data)
-            print(data)
+	s.bind((HOST, PORT))
+	s.listen()
+	while True:
+		print("Waiting For connection")
+		conn, addr = s.accept()
+		with conn:
+			print('Connected by', addr)
+			while True:
+				data = conn.recv(1024).decode()
+				if not data:
+					break
+				HandleRequest(conn,data)
